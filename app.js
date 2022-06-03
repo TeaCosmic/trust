ce80646702475cbbe4fa7d237e83acaf92ec8b42
TeaCosmic <p.dogru@outlook.com>
2022-06-03 23:17:01 +0200

    Base Setup

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 var static = require('node-static');
 var http = require('http');
 
-var file = new(static.Server)(__dirname);
+var file = new (static.Server)(__dirname);
 
 http.createServer(function (req, res) {
   file.serve(req, res);
